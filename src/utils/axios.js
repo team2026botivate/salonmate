98411a982f63515e444fb7fb2924551b3efd9448
mozxammil <mdmozammil605@gmail.com>
2026-01-14 18:10:51 +0530

    added the credintial code

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,6 +5,7 @@ const BASE_URL = import.meta.env?.VITE_BACKEND_API || 'http://localhost:3003/api
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
+  withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
   },
